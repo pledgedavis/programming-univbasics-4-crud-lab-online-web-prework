ac0e0585cf2c475099bb29d295a1ec40ba2741fb
pledgedavis <pledgedavis14@gmail.com>
2019-11-17 21:21:39 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,40 @@
 def create_an_empty_array
-  
+ create_an_empty_array=[]
 end
 
 def create_an_array
-  
+  create_an_array=['pledge','davis','is','amazing']
 end
 
 def add_element_to_end_of_array(array, element)
+ create_an_array << ("arrays!")
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift('wow')
 end
 
 def remove_element_from_end_of_array(array)
-  
+create_an_array="arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array='wow'
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index="am"
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array='wow'
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array='arrays!'
 end
 
 def update_element_from_index(array, index_number, element)
-
+update_element_from_index='totally'
 end
